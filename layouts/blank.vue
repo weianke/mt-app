763b734d39133aaa212b494ec7f6b645a285814b
anke <245783065@qq.com>
2019-01-30 11:53:01 +0800

    registerOrLogin

---
+++ layouts/blank.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="layout-blank">
+    <nuxt/>
+  </div>
+</template>
+
+<script  type='text/ecmascript-6'>
+export default {
+
+}
+</script>
+
+<style scoped lang='scss'>
+
+</style>
