9933a0e08c8d256649738ed8f3a1adf28222d70f
anke <245783065@qq.com>
2019-01-30 17:35:20 +0800

    router

--- server/index.js
+++ server/index.js
@@ -2,6 +2,15 @@ import Koa from 'koa'
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 
+import mongoose from 'mongoose'
+import bodyParser from 'koa-bodyparser'
+import session from 'koa-generic-session'
+import Redis from 'koa-redis'
+import json from 'koa-json'
+import dbConfig from './dbs/config'
+import passport from './interface/utils/passport'
+import users from './interface/users'
+
 const app = new Koa()
 
 // Import and Set Nuxt.js options
@@ -17,11 +26,35 @@ async function start() {
     port = process.env.PORT || 3000
   } = nuxt.options.server
 
+  app.keys = ['mt', 'keyskeys']
+  app.proxy = true
+  app.use(session({
+    key: 'mt',
+    prefix: 'mt:uid',
+    store: new Redis()
+  }))
+
+  app.use(bodyParser({
+    extendTypes: ['json', 'form', 'text']
+  }))
+
+  app.use(json())
+
+  mongoose.connect(dbConfig.dbs, {
+    useNewUrlParser: true
+  })
+
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+
   // Build in development
   if (config.dev) {
     const builder = new Builder(nuxt)
     await builder.build()
   }
+
+  app.use(users.routes()).use(users.allowedMethods())
 
   app.use(ctx => {
     ctx.status = 200
