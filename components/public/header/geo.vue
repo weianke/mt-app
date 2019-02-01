68e1b956f52569c5cafef1355625cfac7fbc05c3
anke <245783065@qq.com>
2019-02-01 19:38:50 +0800

    store

--- components/public/header/geo.vue
+++ components/public/header/geo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-geo">
-    <i class="el-icon-location" />北京市
+    <i class="el-icon-location" />{{city}}
     <nuxt-link class="changeCity"
                to='/changeCity'>切换城市</nuxt-link>
     [香河 廊坊 上海]
@@ -9,6 +9,11 @@
 
 <script  type='text/ecmascript-6'>
 export default {
+  computed: {
+    city () {
+      return this.$store.state.geo.position.city ? this.$store.state.geo.position.city : '开封市'
+    }
+  }``
 }
 </script>
 
---
+++ store/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import geo from './modules/geo'
+
+Vue.use(Vuex)
+
+const store = () => new Vuex.Store({
+  modules: {
+    geo
+  },
+  actions: {
+    async nuxtServerInit({
+      commit
+    }, {req, app}) {
+      const {
+        status,
+        data: {
+          province,
+          city,
+          ip
+        }
+      } = await app.$axios.get('/geo/getPosition')
+      commit('geo/setPosition', status === 200 ? {
+        city,
+        province,
+        ip
+      } : {
+        city: '',
+        province: '',
+        ip: ''
+      })
+    }
+  }
+})
+
+export default store
