20c5ae792ed82ae3c1e610a49b32b93895821370
anke <245783065@qq.com>
2019-02-03 13:57:16 +0800

    menu2

--- store/index.js
+++ store/index.js
@@ -1,35 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import geo from './modules/geo'
+import home from './modules/home'
 
 Vue.use(Vuex)
 
 const store = () => new Vuex.Store({
   modules: {
-    geo
+    geo,
+    home
   },
   actions: {
     async nuxtServerInit({
       commit
     }, {req, app}) {
-      const {
-        status,
-        data: {
-          province,
-          city,
-          ip
-        }
-      } = await app.$axios.get('/geo/getPosition')
-      console.log(ip)
-      commit('geo/setPosition', status === 200 ? {
-        city,
-        province,
-        ip
-      } : {
-        city: '',
-        province: '',
-        ip: ''
-      })
+      const {status, data: {province, city, ip}} = await app.$axios.get('/geo/getPosition')
+      commit('geo/setPosition', status === 200 ? {city, province, ip} : {city: '', province: '', ip: ''})
+      // const {status: status2, data: {menu}} = await app.$axios.get('geo/menu')
+      // commit('home/setMenu', status2 === 200 ? menu : [])
     }
   }
 })
