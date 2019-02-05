8a6935b391eb794bde51c1cae4ad86f1153e32f7
anke <245783065@qq.com>
2019-02-05 10:39:34 +0800

    a

--- components/public/header/searchbar.vue
+++ components/public/header/searchbar.vue
@@ -3,8 +3,8 @@
     <el-row class="m-header-searchbar">
       <el-col :span="3"
               class="left">
-        <img src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/logo.png"
-             alt="美团">
+        <a href="/"><img src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/logo.png"
+             alt="美团"></a>
       </el-col>
       <el-col :span="15"
               class="center">
