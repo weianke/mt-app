ea4ea6fd08d1ceca52f1d4a9d46e22b93f1686e5
anke <245783065@qq.com>
2019-02-09 10:59:59 +0800

    地图组件完成

---
+++ components/public/map.vue
@@ -0,0 +1,69 @@
+<template>
+  <div :id="id"
+       :style="{width: width+'px', height: height+'px', margin: '34px auto'}"
+       class="m-map"></div>
+</template>
+
+<script  type='text/ecmascript-6'>
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 300
+    },
+    height: {
+      type: Number,
+      default: 300
+    },
+    point: {
+      type: Array,
+      default () {
+        return [116.46, 39.92]
+      }
+    }
+  },
+  data () {
+    return {
+      id: `map`,
+      key: '641ff636d6e51d0722a4bd406c3f4959'
+    }
+  },
+  watch: {
+    point (val, old) {
+      this.map.setCenter(val)
+      this.marker.setPosition(val)
+    }
+  },
+  mounted () {
+    let self = this
+    self.id = `map${Math.random().toString().slice(4, 6)}`
+
+    window.onmaploaded = () => {
+      let map = new window.AMap.Map(self.id, {
+        resizeEnable: true,
+        zoom: 11,
+        center: self.point
+      })
+      self.map = map
+      window.AMap.plugin('AMap.ToolBar', () => {
+        let toolbar = new window.AMap.ToolBar()
+        map.addControl(toolbar)
+        let marker = new window.AMap.Marker({
+          icon: 'https://webapi.amap.com/theme/v1.3/markers/n/mark_b.png',
+          position: self.point
+        })
+        self.marker = marker
+        marker.setMap(map)
+      })
+    }
+    const url = `https://webapi.amap.com/maps?v=1.4.10&key=${self.key}&callback=onmaploaded`
+    let jsapi = document.createElement('script')
+    jsapi.charset = 'utf-8'
+    jsapi.src = url
+    document.head.appendChild(jsapi)
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+</style>
