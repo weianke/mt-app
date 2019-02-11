c444efef77efa0cebf282ec44a21e3edbb7ed326
anke <245783065@qq.com>
2019-02-11 16:17:45 +0800

    产品列表页完成

--- pages/products.vue
+++ pages/products.vue
@@ -32,22 +32,51 @@ export default {
       list: [],
       types: [],
       areas: [],
-      keyword: ''
+      keyword: '',
+      point:[]
     }
   },
   async asyncData (ctx) {
-    let keyword = encodeURIComponent(ctx.query.keyword)
-    let city = ctx.store.state.geo.position.city.length ? encodeURIComponent(ctx.store.state.geo.position.city) : '北京'
-    let {status, data: {count, pois}} = await ctx.$axios.get('/search/resultsByKeywords', {
+    let keyword = ctx.query.keyword
+    let city = ctx.store.state.geo.position.city.length ? ctx.store.state.geo.position.city : '北京'
+    let { status, data: { count, pois } } = await ctx.$axios.get('/search/resultsByKeywords', {
       params: {
         keyword,
         city
       }
     })
+    let { status: status2, data: { areas, types } } = await ctx.$axios.get('/categroy/crumbs', {
+      params: {
+        city
+      }
+    })
+    if (status === 200 && count > 0 && status2 == 200) {
+      return {
+        list: pois.filter(item => item.photos.length).map(item => {
+          return {
+            type: item.type,
+            img: item.photos[0].url,
+            name: item.name,
+            comment: Math.floor(Math.random() * 10000),
+            rate: Number(item.biz_ext.rating),
+            price: Number(item.biz_ext.cost),
+            scene: item.tag,
+            tel: item.tel,
+            status: '可订明日',
+            location: item.location,
+            module: item.type.split(';')[0]
+          }
+        }),
+        keyword,
+        areas: areas.filter(item => item.type !== '').slice(0, 5),
+        types: types.filter(item => item.type !== '').slice(0, 5),
+        point: (pois.find(item => item.location).location || '').split(',')
+      }
+    }
   }
 }
 </script>
 
-<style scoped lang='scss'>
-  @import "@/assets/css/products/index.scss";
+<style  lang='scss'>
+ @import "@/assets/css/products/index.scss";
 </style>
