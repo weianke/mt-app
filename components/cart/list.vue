f5e018f29aca72a3bf0bd2716e13f6b85c213a7d
anke <245783065@qq.com>
2019-02-17 20:18:59 +0800

    cart

---
+++ components/cart/list.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-table :data="cartData"
+            style="width: 980px">
+    <el-table-column label="项目"
+                     prop="name"
+                     width="532" />
+    <el-table-column label="单价"
+                     prop="price"
+                     width="132" />
+    <el-table-column label="数量"
+                     width="212">
+      <template slot-scope="scope">
+        <el-input-number v-model="scope.row.count"
+                         :min="0"></el-input-number>
+      </template>
+    </el-table-column>
+    <el-table-column label="总价">
+       <template slot-scope="scope">
+         <div class="">
+           {{scope.row.price * scope.row.count}}
+         </div>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script  type='text/ecmascript-6'>
+export default {
+  props: {
+    cartData: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+</style>
---
+++ pages/cart.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="page-cart">
+    <el-row>
+      <el-col :span="24"
+              v-if="1 || cart.length"
+              class="m-cart">
+        <List />
+        <p>应付金额：
+          <em class="money">￥{{total}}</em>
+        </p>
+        <div class="post">
+          <el-button type="primary" @click="submit">提交订单</el-button>
+        </div>
+      </el-col>
+      <el-col :span="24"
+              class="empty"
+              v-else>购物车为空</el-col>
+    </el-row>
+  </div>
+</template>
+
+<script  type='text/ecmascript-6'>
+import List from '@/components/cart/list'
+export default {
+  components: {
+    List
+  },
+  data () {
+    return {
+      cart: []
+    }
+  },
+  computed: {
+    total () {
+      let total = 0
+      this.cart.forEach(item => {
+        total += item.price * item.count
+      })
+      return total
+    }
+  },
+  methods: {
+    submit () {
+      
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+@import '@/assets/css/cart/index.scss';
+</style>
