85c0cc1b261e557fa4d2bbe99560badfb72d935e
anke <245783065@qq.com>
2019-02-20 18:15:07 +0800

    order

---
+++ components/order/list.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script  type='text/ecmascript-6'>
+export default {
+  props: {
+    cur: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+
+</style>
---
+++ pages/order.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="page-order">
+    <el-row>
+      <el-col :span="4" class="navbar">
+        <h3>我的美团</h3>
+        <dl>
+          <dt>我的订单</dt>
+          <dd>全部订单<i class="el-icon-arrow-right"></i></dd>
+          <dd>待付款<i class="el-icon-arrow-right"></i></dd>
+          <dd>待使用<i class="el-icon-arrow-right"></i></dd>
+        </dl>
+        <dl>
+          <dt>我的收藏</dt>
+          <dd>收藏的商家<i class="el-icon-arrow-right"></i></dd>
+          <dd>收藏的团购<i class="el-icon-arrow-right"></i></dd>
+        </dl>
+        <dl>
+          <dt>抵用券</dt>
+          <dd>可用券<i class="el-icon-arrow-right"></i></dd>
+          <dd>失效劵<i class="el-icon-arrow-right"></i></dd>
+        </dl>
+      </el-col>
+      <el-col :span="19" class="table">
+         <el-tabs v-model="activeName" @tab-click="handleClick">
+          <el-tab-pane label="全部订单" name="all">
+            <list :cur="cur"/>
+          </el-tab-pane>
+          <el-tab-pane label="待付款" name="unpay">
+             <list :cur="cur"/>
+          </el-tab-pane>
+          <el-tab-pane label="待使用" name="unse">
+             <list :cur="cur"/>
+          </el-tab-pane>
+          <el-tab-pane label="待评价" name="unreplay">
+             <list :cur="cur"/>
+          </el-tab-pane>
+        </el-tabs>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script  type='text/ecmascript-6'>
+import List from '@/components/order/list'
+export default {
+  components: {
+    List
+  },
+  data() {
+      return {
+        activeName: 'all',
+        list: [],
+        cur: []
+      }
+    },
+    methods: {
+      handleClick(tab, event) {
+        this.activeName = tab.name
+      }
+    },
+    watch: {
+      activeName (val) {
+        this.cur = this.list.filter(item => {
+          if (val === 'unpay') {
+            return item.status === 0
+          } else if (val === 'all') {
+            return true
+          } else {
+            return false
+          }
+        })
+      },
+      list () {
+        let val = this.name
+         this.cur = this.list.filter(item => {
+          if (val === 'unpay') {
+            return item.status === 0
+          } else if (val === 'all') {
+            return true
+          } else {
+            return false
+          }
+        })
+      }
+    },
+    async asyncData (ctx) {
+
+    }
+}
+</script>
+
+<style lang='scss'>
+ @import "@/assets/css/order/index.scss";
+</style>
