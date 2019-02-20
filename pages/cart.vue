d362eb2b429ae280667f9831890852d7720604f2
anke <245783065@qq.com>
2019-02-20 11:00:46 +0800

    shopCartEnd

--- pages/cart.vue
+++ pages/cart.vue
@@ -2,14 +2,15 @@
   <div class="page-cart">
     <el-row>
       <el-col :span="24"
-              v-if="1 || cart.length"
+              v-if="cart.length"
               class="m-cart">
-        <List />
+        <List :cart-data="cart" />
         <p>应付金额：
           <em class="money">￥{{total}}</em>
         </p>
         <div class="post">
-          <el-button type="primary" @click="submit">提交订单</el-button>
+          <el-button type="primary"
+                     @click="submit">提交订单</el-button>
         </div>
       </el-col>
       <el-col :span="24"
@@ -40,8 +41,35 @@ export default {
     }
   },
   methods: {
-    submit () {
-      
+    async submit () {
+      let { status, data: { code, id } } = await this.$axios.post('/order/createOrder', {
+        count: this.cart[0].count,
+        price: this.cart[0].price,
+        id: this.cartNo
+      })
+      if (status == 200 && code === 0) {
+        this.$alert(`恭喜您，已成功下单，订单号:${id}`, '下单成功', {
+          confirmButtonText: '确定',
+          callback: action => {
+            location.href = '/order'
+          }
+        })
+      }
+    }
+  },
+  async asyncData (ctx) {
+    let { status, data: { code, data: { name, price } } } = await ctx.$axios.post('/cart/getCart', {
+      id: ctx.query.id
+    })
+    if (status === 200 && code === 0 && name) {
+      return {
+        cart: [{
+          name,
+          price,
+          count: 1
+        }],
+        cartNo: ctx.query.id
+      }
     }
   }
 }
--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ import users from './interface/users'
 import geo from './interface/geo'
 import search from './interface/search'
 import categroy from './interface/categroy'
+import cart from './interface/cart'
+
 
 const app = new Koa()
 
@@ -61,6 +63,8 @@ async function start() {
   app.use(geo.routes()).use(geo.allowedMethods())
   app.use(search.routes()).use(search.allowedMethods())
  app.use(categroy.routes()).use(categroy.allowedMethods())
+ app.use(cart.routes()).use(cart.allowedMethods())
+ 
   app.use(ctx => {
     ctx.status = 200
     ctx.respond = false // Bypass Koa's built-in response handling
